aa8213951ff3fd682786e031c362993a1a92cc2f
Aaron Chris <rooneychris83@gmail.com>
2022-08-22 18:43:02 +0100

    Added map method syntax

--- map.js
+++ map.js
@@ -5,3 +5,32 @@ const mapResult = toMultiply.map(x => x * 2);
 
 console.log(mapResult);
 // expected output: Array [2, 8, 18, 32]
+
+
+
+
+
+
+// Syntax
+.
+.
+.
+.
+.
+.
+.
+.
+// Arrow function
+map((currentValue) => { ... } )
+map((currentValue, index) => { ... } )
+map((currentValue, index, array) => { ... } )
+
+// Callback function
+map(callbackFn)
+map(callbackFn, thisArg)
+
+// Inline callback function
+map(function callbackFn(currentValue) { ... })
+map(function callbackFn(currentValue, index) { ... })
+map(function callbackFn(currentValue, index, array){ ... })
+map(function callbackFn(currentValue, index, array) { ... }, thisArg)
